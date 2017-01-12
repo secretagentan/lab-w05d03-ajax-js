2f33566a9e1aa6449c23648c023b69079640bbfb
an nguyen <hello.an.ng@gmail.com>
2017-01-11 16:19:25 -0800

    solves challenge 7

--- public/main.js
+++ public/main.js
@@ -2,14 +2,14 @@ console.log("May the odds be ever in your favor");
 
 var $button = $('#ready');
 var $challenge = $('#challenges');
-var $moar = "<div id='moar'></div>";
-var $nextButton = "<button id='next'>Next</button>";
+// var $moar = "<div id='moar'></div>";
+// var $nextButton = "<button id='next'>Next</button>";
 
 function handleButton(evt) {
   $.get('/challenges', function(res) {
     var $list = '<ul>';
     res.forEach( function(val) {
-      return $list += '<li>' + val.id + ': ' + val.body + '</li>';
+      return $list += '<li>' + val.id + '. ' + val.body + '</li>';
     })
     $list += '</ul>';
     $challenge.append($list);
@@ -25,6 +25,7 @@ function createElems() {
   $challenge.after("<div id='moar'></div>");
   $nextButton = $('#next');
   $moar = $('#moar');
+  $moar.on('click', secret);
 }
 
 function nextTwo(evt) {
@@ -39,8 +40,63 @@ function handleRes(res) {
 function concat(arr) {
   var $list = '<ul>';
   arr.forEach(function(val){
-    return $list += '<li>' + val.id + ': ' + val.body + '</li>';
+    return $list += '<li>' + val.id + '. ' + val.body + '</li>';
   })
   $list += '</ul>';
   return $list;
 };
+
+// Challenge #7
+
+function secret(evt) {
+  var targ = evt.target;
+  // console.log('secret');
+  // console.log(targ);
+  if ( targ === $moar.find('li')[2]) {
+    // console.log(targ);
+    $.ajax({
+      url:'https://mighty-caverns-93139.herokuapp.com/help',
+      headers: {
+        'x-secret': 'shh',
+      },
+      type: 'GET',
+      success: function(msg) {
+         console.log(msg);
+         $moar.children().eq(1).append('<li style="color:red"> Psst: ' + msg.message + '</li>');
+      }
+    })
+    $.ajax({
+      url: 'https://mighty-caverns-93139.herokuapp.com/solution',
+      type: 'POST',
+      data: {
+        'answer' : 'CORS'
+      },
+      success: function(msg) {
+        console.log(msg);
+        $moar.children().eq(2).append('<li style="color:green"> Hey! ' + msg.data.hint + '<br>' + msg.data.message + '</li>');
+      },
+      error: function(msg) {
+        console.log('failure');
+      }
+    })
+  }
+}
+
+var decodedSix = window.atob('SW4gPGNvZGU+cHVibGljL21haW4uanM8L2NvZGU+IHVwZGF0ZSA8Y29kZT5yZW5kZXJDaGFsbGVuZ2U8L2NvZGU+IHRvIG91dHB1dCA8Y29kZT4mbHQ7YSBocmVmPSIvY2hhbGxlbmdlcy9baWRdIiZndDtbQ2hhbGxlbmdlXSZsdDsvYSZndDs8L2NvZGU+');
+var decodedEight = window.atob('T24gPGNvZGU+c2VydmVyLmpzPC9jb2RlPiwgaGFuZGxlIEdFVCByZXF1ZXN0cyB0byA8Y29kZT4vY2hhbGxlbmdlcy9baWRdPC9jb2RlPiBhbmQgcmV0dXJuIE9OTFkgdGhhdCBjaGFsbGVuZ2UgYXMgSlNPTg==');
+var decodedNine = window.atob('UmVhZCB0aHJvdWdoIGh0dHBzOi8vZXhwcmVzc2pzLmNvbS9lbi9ndWlkZS91c2luZy10ZW1wbGF0ZS1lbmdpbmVzLmh0bWw=');
+var decodedTen = window.atob('UmVzZWFyY2ggLSB3aGF0IGlzIHRoZSBkZWZhdWx0IGRpcmVjdG9yeSB3aGVyZSBleHByZXNzIGxvb2tzIGZvciB0ZW1wbGF0ZSBmaWxlcz8=');
+
+console.log(decodedSix);
+console.log(decodedEight);
+console.log(decodedNine);
+console.log(decodedTen);
+
+try {
+  window.atob('hello');
+} catch (err) {
+  console.log('haha');
+};
+
+
+
